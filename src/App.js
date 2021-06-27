1d9e3a22ded784c1c34385437fa742475fe3d6c2
Marceloprime <marceloreis7777@gmail.com>
2021-06-27 01:50:46 -0300

    add react navigation

---
+++ src/App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title} >Marketplace</Text>
+    </View>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#f5f5f5"
+  },
+  title:{
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10,
+  }
+})
